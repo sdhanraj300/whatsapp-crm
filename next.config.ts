26a695f4f1108ab049c20c13fb7ac64f387249f4
Dhanraj Singh <sdhanraj300@gmail.com>
2025-06-29 10:52:16 +0530

    5th

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from 'next';
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add any other configurations you need here
+};
+
+export default nextConfig;
